3990219045f60b567d08ee27dc91bb22c452ebee
thumbsu <uumj222@gmail.com>
2021-04-07 16:33:09 +0900

    feat: add monaco-editor component

--- src/App.js
+++ src/App.js
@@ -1,30 +1,20 @@
-import { useMonaco } from "@monaco-editor/react";
-import { useEffect } from "react";
-import { MonacoBinding } from "y-monaco";
+import { Fragment } from "react";
 import { options } from "./editorOptions";
-import { useYjs } from "./hooks/useYjs";
+import MonacoEditor from "./MonacoEditor";
 
 function App() {
-  const monaco = useMonaco();
-  const { type, provider } = useYjs();
-
-  useEffect(() => {
-    if (monaco && provider !== null && type !== null) {
-      const editor = monaco.editor.create(
-        document.getElementById("monaco-editor"),
-        { ...options, language: "javascript" }
-      );
-      monaco.editor.setTheme("vs-dark");
-      new MonacoBinding(
-        type,
-        editor.getModel(),
-        new Set([editor]),
-        provider.awareness
-      );
-    }
-  }, [monaco, provider, type]);
-
-  return <div id="monaco-editor" style={{ height: "100vh" }}></div>;
+  return (
+    <Fragment>
+      <div style={{ height: "100vh" }}>
+        <MonacoEditor
+          value="// some code"
+          language="javascript"
+          options={options}
+          theme="vs-dark"
+        />
+      </div>
+    </Fragment>
+  );
 }
 
 export default App;
